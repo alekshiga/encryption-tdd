a6e57e72536c5c36b2d8b796b7c6c26bf6cb2c86
alekshiga <alekshiga@vk.com>
2026-02-28 12:24:52 +0700

    Красный: метод encrypt в классе Encryption не реализован

--- tests/testencryption.py
+++ tests/testencryption.py
@@ -12,5 +12,11 @@ class TestEncryption(unittest.TestCase):
         encryption = Encryption()
         self.assertIsNotNone(encryption)
 
+    def test_caesar_single_letter_encryption(self):
+        encryption = Encryption()
+        result = encryption.encrypt('a', 3)
+        # 1 - a, 2 - b, 3 - c, 4 - d
+        self.assertEqual(result, 'd')
+
 if __name__ == '__main__':
     unittest.main()
