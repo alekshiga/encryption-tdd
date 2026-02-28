23c50dcc55165240a67dbd5c4e70518af385b183
alekshiga <alekshiga@vk.com>
2026-02-28 12:41:08 +0700

    Синий: рефакторинг, сдвиг теперь в отдельном приватном методе

--- encryption/encryption.py
+++ encryption/encryption.py
@@ -4,26 +4,19 @@ class Encryption:
         # сделаем заглушку чтобы проходил тест
         pass
 
+    # приватный метод для сдвига todo что-нибудь посложнее потом можно будет придумать
+    @staticmethod
+    def _shift(char, shift):
+        if char.isupper():
+            return chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
+        elif char.islower():
+            return chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+        return char
+
     def encrypt(self, text, shift):
-        # обычный сдвиг шифром Цезаря, todo сделать что-нибудь посложнее
-        result = []
-        for char in text:
-            if char.isupper():
-                result.append(chr((ord(char) - ord('A') + shift) % 26 + ord('A')))
-            elif char.islower():
-                result.append(chr((ord(char) - ord('a') + shift) % 26 + ord('a')))
-            else:
-                result.append(char)
+        result = [self._shift(char, shift) for char in text]
         return ''.join(result)
 
     def decrypt(self, text, shift):
         # расшифровка шифра
-        result = []
-        for char in text:
-            if char.isupper():
-                result.append(chr((ord(char) - ord('A') - shift) % 26 + ord('A')))
-            elif char.islower():
-                result.append(chr((ord(char) - ord('a') - shift) % 26 + ord('a')))
-            else:
-                result.append(char)
-        return ''.join(result)
+        return self.encrypt(text, 26 - (shift % 26))
