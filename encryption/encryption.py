7bac0ef782170fd917e3fb963c29fdf7a9f6e86b
alekshiga <alekshiga@vk.com>
2026-02-28 12:27:16 +0700

    Зелёный: шифрование работает как нужно

--- encryption/encryption.py
+++ encryption/encryption.py
@@ -3,3 +3,15 @@ class Encryption:
     def __init__(self):
         # сделаем заглушку чтобы проходил тест
         pass
+
+    def encrypt(self, text, shift):
+        # обычный сдвиг шифром Цезаря, todo сделать что-нибудь посложнее
+        result = []
+        for char in text:
+            if char.isupper():
+                result.append(chr((ord(char) - ord('A') + shift) % 26 + ord('A')))
+            elif char.islower():
+                result.append(chr((ord(char) - ord('a') + shift) % 26 + ord('a')))
+            else:
+                result.append(char)
+        return ''.join(result)
