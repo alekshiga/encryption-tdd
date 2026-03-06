52878298cd3128f2ca3ac1f16e762d54eaace44c
alekshiga <alekshiga@vk.com>
2026-03-06 21:09:13 +0700

    Зелёный: теперь шифратор работает с цифрами, строчными и заглавными буквами

--- encryption/encryption.py
+++ encryption/encryption.py
@@ -1,7 +1,35 @@
 class Encryption:
 
     def encrypt(self, text):
-        return "".join(chr(ord(c) + 1) for c in text)
+        result = ""
+        for c in text:
+            if 'a' <= c <= 'y' or 'A' <= c <= 'Y':
+                result += chr(ord(c) + 1)
+            elif c == 'z':
+                result += 'a'
+            elif c == 'Z':
+                result += 'A'
+            elif '0' <= c <= '8':
+                result += chr(ord(c) + 1)
+            elif c == '9':
+                result += '0'
+            else:
+                result += c
+        return result
 
     def decrypt(self, text):
-        return "".join(chr(ord(c) - 1) for c in text)
+        result = ""
+        for c in text:
+            if 'b' <= c <= 'z' or 'B' <= c <= 'Z':
+                result += chr(ord(c) - 1)
+            elif c == 'a':
+                result += 'z'
+            elif c == 'A':
+                result += 'Z'
+            elif '1' <= c <= '9':
+                result += chr(ord(c) - 1)
+            elif c == '0':
+                result += '9'
+            else:
+                result += c
+        return result
