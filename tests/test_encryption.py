11aeaba93929dc2e75ec24a423ef74d29932c83e
alekshiga <alekshiga@vk.com>
2026-03-06 21:08:25 +0700

    Красный: тест работы с цифрами падает, т.к. нет такого функционала

--- tests/test_encryption.py
+++ tests/test_encryption.py
@@ -14,5 +14,11 @@ class TestEncryption(unittest.TestCase):
         result = encryption.decrypt("bcd")
         self.assertEqual(result, "abc")
 
+    # тест работы с цифрами
+    def test_encrypt_digits(self):
+        encryption = Encryption()
+        result = encryption.encrypt("123")
+        self.assertEqual(result, "234")
+
 if __name__ == "__main__":
     unittest.main()
