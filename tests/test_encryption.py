8e9e029f26ab51e71852165905f3a96d6256617d
alekshiga <alekshiga@vk.com>
2026-03-06 20:18:43 +0700

    Красный: тест на создание объекта Encryption

---
+++ tests/test_encryption.py
@@ -0,0 +1,14 @@
+import unittest
+from encryption.encryption import Encryption
+
+
+class TestEncryption(unittest.TestCase):
+
+    def test_encrypt_shifts_letters_by_one(self):
+        encryption = Encryption()
+        result = encryption.encrypt("abc")
+        self.assertEqual(result, "bcd")
+
+
+if __name__ == "__main__":
+    unittest.main()
